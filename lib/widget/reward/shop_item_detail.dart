a1790560e926735138621632d67f8d9ee1a733dc
Sean Wen <sw92410@hotmail.com>
2022-01-30 15:05:12 -0700

    Fix item detail image display

--- lib/widget/reward/shop_item_detail.dart
+++ lib/widget/reward/shop_item_detail.dart
@@ -14,16 +14,29 @@ class _ShopItemDetailState extends State<ShopItemDetail> {
       extendBodyBehindAppBar: true,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
-        shadowColor: Colors.black.withOpacity(0.5),
-        title: const Text("Shake and Eat"),
+        // backgroundColor: Colors.white.withOpacity(0.5),
+        // shadowColor: Colors.transparent,
+        shadowColor: Colors.black.withOpacity(0.3),
         centerTitle: true,
       ),
       body: Expanded(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Image.network(
-              "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/recipe-image-legacy-id-1040464_11-6d03a3e.jpg",
-              fit: BoxFit.cover,
+            ClipRRect(
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(20),
+                bottomRight: Radius.circular(20),
+              ),
+              child: Image(
+                // alignment: Alignment.topCenter,
+                width: double.infinity,
+                height: 200,
+                fit: BoxFit.fitWidth,
+                image: NetworkImage(
+                  "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/recipe-image-legacy-id-1040464_11-6d03a3e.jpg",
+                ),
+              ),
             ),
             Text("item description "),
           ],
