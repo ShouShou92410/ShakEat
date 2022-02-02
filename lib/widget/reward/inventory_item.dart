74e24afd3d967a53553a62811269f73e77686b9e
Sean Wen <sw92410@hotmail.com>
2022-02-01 20:32:42 -0700

    Add inventory item and item detail files

---
+++ lib/widget/reward/inventory_item.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class InventoryItem extends StatefulWidget {
+  const InventoryItem({Key? key}) : super(key: key);
+
+  @override
+  _InventoryItemState createState() => _InventoryItemState();
+}
+
+class _InventoryItemState extends State<InventoryItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
